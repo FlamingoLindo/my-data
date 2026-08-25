f0e596131b7647881d33c462dbed18a05ba29580
flamingo_lindo <vitorantunes2003@gmail.com>
2026-08-25 14:37:26 -0300

    init

---
+++ src/lib/components/login-form.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import * as Card from "$lib/components/ui/card/index.js";
+  import { Button } from "$lib/components/ui/button/index.js";
+  import {
+    FieldGroup,
+    Field,
+    FieldLabel,
+    FieldDescription,
+  } from "$lib/components/ui/field/index.js";
+  import { Input } from "$lib/components/ui/input/index.js";
+
+  const id = $props.id();
+</script>
+
+<Card.Root class="mx-auto w-full max-w-sm">
+  <Card.Content>
+    <form>
+      <FieldGroup>
+        <Field>
+          <FieldLabel for="email-{id}">Email</FieldLabel>
+          <Input id="email-{id}" type="email" required />
+        </Field>
+        <Field>
+          <div class="flex items-center">
+            <FieldLabel for="password-{id}">Password</FieldLabel>
+            <!-- <a href="##" class="ms-auto inline-block text-sm underline"> Forgot your password? </a> -->
+          </div>
+          <Input id="password-{id}" type="password" required />
+        </Field>
+        <Field>
+          <Button type="submit" class="w-full">Login</Button>
+        </Field>
+      </FieldGroup>
+    </form>
+  </Card.Content>
+</Card.Root>
