7bfd67a56739be74dcba51e3905f371917114064
flamingo_lindo <vitorantunes2003@gmail.com>
2026-09-14 10:46:46 -0300

    feat: remove nginx

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,16 @@
-import { MY_USERNAME, MY_PASSWORD, AUTH_SECRET } from '$env/static/private';
+import { MY_USERNAME, MY_PASSWORD, AUTH_SECRET, WHITELISTEDIPS } from '$env/static/private';
 import { fail, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
 
 export const actions: Actions = {
-    default: async ({ request, cookies }) => {
+    default: async ({ request, cookies, getClientAddress }) => {
+        const ip = getClientAddress().replace("::ffff:", "");
+        const allowed = WHITELISTEDIPS.split(',').map(i => i.trim()).filter(Boolean);
+
+        if (!allowed.includes(ip)) {
+            return redirect(303, 'https://www.youtube.com/watch?v=p-Bptisac1A?autoplay=1');
+        }
+
         const data = await request.formData();
         const username = data.get('username');
         const password = data.get('password');
